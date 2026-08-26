5d331bbfda08518c704ef6e370489cfc1ccd9a15
joshuacharley <34540432+joshuacharley@users.noreply.github.com>
2026-08-26 16:08:29 +0000

    fix app closes issues

--- android/app/src/main/java/com/alphateknexus/app/MainActivity.java
+++ android/app/src/main/java/com/alphateknexus/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.alphateknexus.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import androidx.core.splashscreen.SplashScreen;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+    }
+}
